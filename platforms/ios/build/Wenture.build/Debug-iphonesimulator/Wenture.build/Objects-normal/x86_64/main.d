ded100bf7ca31fd9b62170fddf478aa3f6fe842f
rennehir <git@rennehir.fi>
2017-03-30 12:03:16 +0300

    Initial commit

---
+++ platforms/ios/build/Wenture.build/Debug-iphonesimulator/Wenture.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/main.m \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/renne/Documents/code/Wenture/platforms/ios/internal/TNSDebugging.h
