a889e473c30df24148cf8d075a533d2074fda80e
rennehir <git@rennehir.fi>
2017-05-04 22:35:59 +0300

    Updated prizeservice

--- app/pages/map-page/prize-view.ts
+++ app/pages/map-page/prize-view.ts
@@ -2,10 +2,13 @@ import { Component, OnInit, NgModule } from "@angular/core";
 import { ModalDialogParams } from "nativescript-angular/modal-dialog";
 import { Page } from "ui/page";
 import { Label } from "ui/label";
+import { PrizeService } from "../../shared/prize/prize.service";
+import { WenturePointService } from "../../shared/wenturepoint/wenturepoint.service";
 
 // >> passing-parameters
 @Component({
     moduleId: module.id,
+    providers: [PrizeService, WenturePointService],
     templateUrl: "./prize-view.html",
     styleUrls: ["./prize-view-common.css", "./prize-view.css"]
 })
@@ -13,14 +16,18 @@ export class PrizeViewComponent implements OnInit {
   wenturePointTitle: string;
   marker: any;
 
-  constructor(private params: ModalDialogParams, private page: Page) {
+  constructor(private params: ModalDialogParams, private page: Page, private prizeService: PrizeService, private wenturePointService: WenturePointService) {
       this.marker = params.context;
+      this.setTextViews();
   }
 
   ngOnInit() {
     this.page.backgroundImage = "res://loginbg";
     this.wenturePointTitle = this.marker.title;
-    console.log("ngOnInit");
+  }
+
+  setTextViews() {
+    
   }
 
   public submit() {
