378f43499bed1ad2c3117cb5eec6e8403b730740
Miiro Korhonen <Miiro Korhonen>
2017-05-05 02:25:36 +0300

    Prizelist DOESN'T WORK!  but prize.view.android.css file is now ready

---
+++ app/pages/list/list.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Page } from "ui/page";
+//import { Prize } from "../../shared/prize/prize";
+//import { PrizeService } from "../../shared/prize/prize.service";
+
+@Component({
+  selector: "list",
+  //providers: [PrizeService],
+  templateUrl: "pages/list/list.html",
+  styleUrls: ["pages/list/list-common.css", "pages/list/list.css"]
+})
+export class ListComponent implements OnInit {
+  //prizeList: Array<Object> = [];
+
+  constructor(/*private prizeService: PrizeService, */private page: Page) {}
+
+  ngOnInit() {
+    //this.prizeList.push({ name: "Prize1" });
+    //this.prizeList.push({ name: "Prize2" });
+    //this.prizeList.push({ name: "Prize3" });
+  }
+
+  /*ngOnInit() {
+  this.isLoading = true;
+  this.prizeListService.load()
+    .subscribe(loadedPrizes => {
+      loadedPrizes.forEach((prizeObject) => {
+        this.prizeList.unshift(prizeObject);
+      });
+      this.isLoading = false;
+      this.listLoaded = true;
+    });*/
+}
